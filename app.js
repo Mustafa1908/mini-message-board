474472724255e0a0f4ac62f701f6ba40ffaef479
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-08-18 20:03:18 +0200

    Add css to ejs

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 const createMessage = require("./routes/index");
+const assetsPath = path.join(__dirname, "public/css");
 
+app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
