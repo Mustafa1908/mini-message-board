86cec3d1390c368de588500c9ecf9fe2062bdce4
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-08-18 17:30:45 +0200

    Change variable name

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const path = require("node:path");
 const express = require("express");
 const app = express();
-const newMessage = require("./routes/index");
+const createMessage = require("./routes/index");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", newMessage);
+app.use("/", createMessage);
 
 const PORT = 8080;
 
