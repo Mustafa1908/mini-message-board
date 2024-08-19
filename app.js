30c7dc9f7743a73b6e53c47a9e73d58ba65538fe
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-08-19 14:33:09 +0200

    Change css dirname path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 const createMessage = require("./routes/index");
-const assetsPath = path.join(__dirname, "public/css");
+const assetsPath = path.join(__dirname, "public");
 
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
