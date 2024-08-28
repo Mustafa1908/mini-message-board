e73cd6f9d547837e84a0c6261bbad5578a5dcc02
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-08-28 20:18:59 +0200

    Importe dotenv package, change router path

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+require("dotenv").config();
+
 const path = require("node:path");
 const express = require("express");
 const app = express();
-const createMessage = require("./routes/index");
+const messagesRouter = require("./routes/messageRouter");
 const assetsPath = path.join(__dirname, "public");
 
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", createMessage);
+app.use("/", messagesRouter);
 
-const PORT = 8080;
-
-app.listen(PORT, () =>
-  console.log(`My first Express app - listening on port ${PORT}!`)
-);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
